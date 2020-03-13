26b47da9159612a21b6db9bf3573ac17a2ae8f54
Ramin Valizadeh <r.valizadeh1365@yahoo.com>
2020-03-13 19:34:22 +0330

    Feat : Add safe api call function

---
+++ app/src/main/java/com/themoviedb/helper/safeApiCall.kt
@@ -0,0 +1,35 @@
+package com.themoviedb.helper
+
+import com.google.gson.Gson
+import com.themoviedb.models.ErrorResponse
+import com.themoviedb.models.ResultWrapper
+import retrofit2.HttpException
+import java.io.IOException
+
+suspend fun <T> safeApiCall(apiCall: suspend () -> T): ResultWrapper<T> {
+    return try {
+        ResultWrapper.Success(apiCall.invoke())
+    } catch (throwable: Throwable) {
+        when (throwable) {
+            is IOException -> ResultWrapper.NetworkError
+            is HttpException -> {
+                val code = throwable.code()
+                val errorResponse = convertErrorBody(throwable)
+                ResultWrapper.GenericError(code, errorResponse)
+            }
+            else -> {
+                ResultWrapper.GenericError(null, null)
+            }
+        }
+    }
+}
+
+private fun convertErrorBody(throwable: HttpException): ErrorResponse? {
+    return try {
+        throwable.response()?.errorBody()?.string()?.let {
+            return Gson().fromJson(it, ErrorResponse::class.java)
+        }
+    } catch (exception: Exception) {
+        null
+    }
+}
