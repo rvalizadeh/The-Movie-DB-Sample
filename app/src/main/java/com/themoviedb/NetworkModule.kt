8d68c36d64776ef3400c080097bd54e97cd9cd8a
Ramin Valizadeh <r.valizadeh1365@yahoo.com>
2020-03-13 19:30:09 +0330

    Feat : Add network module

---
+++ app/src/main/java/com/themoviedb/NetworkModule.kt
@@ -0,0 +1,42 @@
+package com.themoviedb
+
+import android.content.Context
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val networkModule = module {
+    factory { AuthInterceptor() }
+    factory { provideOkHttpClient(get(), get()) }
+    single { provideRetrofit(get()) }
+    factory { provideOkHttpLoggerInterceptor() }
+}
+
+
+fun provideOkHttpLoggerInterceptor(): HttpLoggingInterceptor {
+    return HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+}
+
+fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    return Retrofit.Builder().baseUrl(DataConstant.BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+}
+
+fun provideOkHttpClient(
+    authInterceptor: AuthInterceptor,
+    okHttpLoggerInterceptor: HttpLoggingInterceptor
+): OkHttpClient {
+    return OkHttpClient().newBuilder()
+        .addInterceptor(authInterceptor)
+        .addInterceptor(okHttpLoggerInterceptor)
+        .connectTimeout(2 , TimeUnit.MINUTES)
+        .readTimeout(2 , TimeUnit.MINUTES)
+        .build()
+}
+
