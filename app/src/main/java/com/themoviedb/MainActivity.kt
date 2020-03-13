d9f9d32f47404656e475f45a842accb6c107afff
Ramin Valizadeh <r.valizadeh1365@yahoo.com>
2020-03-13 19:48:42 +0330

    Feat : set observable in main activity

--- app/src/main/java/com/themoviedb/MainActivity.kt
+++ app/src/main/java/com/themoviedb/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.themoviedb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import com.themoviedb.base.BaseActivity
+import com.themoviedb.viewmodel.NowPlayingViewModel
+import org.koin.android.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    private val viewModel: NowPlayingViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.nowPlayingResponseLiveData.observe(this , Observer {
+            var dd = it
+        })
+
+        viewModel.errorMessageLiveData.observe(this , Observer {
+            var dd = it
+        })
+
+
     }
 }
