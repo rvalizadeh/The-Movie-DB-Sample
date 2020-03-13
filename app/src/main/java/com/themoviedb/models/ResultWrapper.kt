a50f92197dceb9943086acdf2f984d8a15fa80c2
Ramin Valizadeh <r.valizadeh1365@yahoo.com>
2020-03-13 19:33:42 +0330

    Feat : Add result wrapper for api response

---
+++ app/src/main/java/com/themoviedb/models/ResultWrapper.kt
@@ -0,0 +1,8 @@
+package com.themoviedb.models
+
+sealed class ResultWrapper<out T> {
+    data class Success<out T>(val value: T): ResultWrapper<T>()
+    data class GenericError(val code: Int? = null, val error: ErrorResponse? = null):
+        ResultWrapper<Nothing>()
+    object NetworkError: ResultWrapper<Nothing>()
+}
