d6b7bfa96f284238fcd1da160b370b92e69990c5
Ramin Valizadeh <r.valizadeh1365@yahoo.com>
2020-03-13 19:31:46 +0330

    Feat : Add data constant class

---
+++ app/src/main/java/com/themoviedb/DataConstant.kt
@@ -0,0 +1,14 @@
+package com.themoviedb
+
+class DataConstant{
+   companion object{
+       const val API_TOKEN = "3c588f7b38fcee160b972c3a0d5f5b82"
+       const val API_KEY = "api_key"
+       const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original/"
+       const val BASE_URL = "https://api.themoviedb.org/3/movie/"
+       const val NOW_PLAYING_URL = "now_playing"
+       const val LANGUAGE_KEY = "language"
+       const val LANGUAGE_VALUE = "en-US"
+       const val PAGE_KEY = "page"
+   }
+}
