828cf0815a3e1378320ece456458902d6347b5c2
Ramin Valizadeh <r.valizadeh1365@yahoo.com>
2020-03-14 20:06:51 +0330

    Feat : Parcelable Results class

--- app/src/main/java/com/themoviedb/models/Results.kt
+++ app/src/main/java/com/themoviedb/models/Results.kt
@@ -1,6 +1,8 @@
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
-
+@Parcelize
 data class Results (
 
 	@SerializedName("popularity") val popularity : Double,
@@ -17,4 +19,5 @@ data class Results (
 	@SerializedName("vote_average") val vote_average : Double,
 	@SerializedName("overview") val overview : String,
 	@SerializedName("release_date") val release_date : String
-)
+
+):Parcelable
