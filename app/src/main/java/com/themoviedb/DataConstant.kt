bfdcac8d0fe6cd7856db1618ac6f11154ecb3fda
Ramin Valizadeh <r.valizadeh1365@yahoo.com>
2020-03-15 20:46:15 +0330

    Feat : Load data into list

--- app/src/main/java/com/themoviedb/DataConstant.kt
+++ app/src/main/java/com/themoviedb/DataConstant.kt
@@ -4,7 +4,7 @@ class DataConstant{
    companion object{
        const val API_TOKEN = "3c588f7b38fcee160b972c3a0d5f5b82"
        const val API_KEY = "api_key"
-       const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original/"
+       const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500_and_h282_bestv2/"
        const val BASE_URL = "https://api.themoviedb.org/3/movie/"
        const val NOW_PLAYING_URL = "now_playing"
        const val LANGUAGE_KEY = "language"
--- app/src/main/java/com/themoviedb/NowPlayingAdapter.kt
+++ app/src/main/java/com/themoviedb/NowPlayingAdapter.kt
@@ -2,61 +2,61 @@ package com.themoviedb
 
 import Results
 import android.content.Context
-import android.graphics.Movie
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideContext
 
 
 class NowPlayingAdapter(private var context: Context , private var movies: MutableList<Results>?) : RecyclerView.Adapter<ViewHolder?>() {
 
-    private var isLoadingAdded = false
+    private var isLoadingAdded = true
 
-    protected inner class LoadingVH(itemView: View) : ViewHolder(itemView)
     companion object {
         private const val ITEM = 0
         private const val LOADING = 1
-    }
-
-    fun getMovies(): List<Results>? {
-        return movies
-    }
-
-    fun setMovies(movies: MutableList<Results>?) {
-        this.movies = movies
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         var viewHolder: ViewHolder
         val inflater = LayoutInflater.from(parent.context)
         when (viewType) {
-            ITEM -> viewHolder = getViewHolder(parent, inflater)
-            else -> {
-                val v2: View = inflater.inflate(R.layout.adapter_item_progress, parent, false)
-                viewHolder = LoadingVH(v2)
-            }
-
+            ITEM -> viewHolder = getDataViewHolder(parent, inflater)
+            else -> viewHolder = getLoadingViewHolder(parent, inflater)
         }
         return viewHolder
     }
 
-    private fun getViewHolder(parent: ViewGroup, inflater: LayoutInflater): ViewHolder {
-        val viewHolder: ViewHolder
-        val v1: View = inflater.inflate(R.layout.adapter_item_now_playing, parent, false)
-        viewHolder = MovieVH(v1)
-        return viewHolder
+    private fun getDataViewHolder(parent: ViewGroup, inflater: LayoutInflater): ViewHolder {
+        val view: View = inflater.inflate(R.layout.adapter_item_now_playing, parent, false)
+        return MovieVH(view)
+    }
+
+    private fun getLoadingViewHolder(parent: ViewGroup, inflater: LayoutInflater): ViewHolder {
+        val view: View = inflater.inflate(R.layout.adapter_item_progress, parent, false)
+        return LoadingVH(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val movie = movies!![position]
         when (getItemViewType(position)) {
             ITEM -> {
-                val movieVH = holder as MovieVH?
+                val movieVH = holder as MovieVH
+                movieVH.tvTitle.text = movie.title
+                movieVH.tvReleaseDate.text = movie.release_date
+                movieVH.tvPercent.text = (movie.vote_average * 10).toInt().toString()
+                movieVH.progressBar.progress = (movie.vote_average * 10).toInt()
+                Glide.with(context).load(DataConstant.IMAGE_BASE_URL + movie.backdrop_path)
+                    .into(movieVH.backgroundImage)
             }
             LOADING -> {
+
             }
         }
     }
@@ -69,10 +69,6 @@ class NowPlayingAdapter(private var context: Context , private var movies: Mutab
         return if (position == movies!!.size - 1 && isLoadingAdded) LOADING else ITEM
     }
 
-    /*
-   Helpers
-   _________________________________________________________________________________________________
-    */
     fun add(mc: Results) {
         movies!!.add(mc)
         notifyItemInserted(movies!!.size - 1)
@@ -84,27 +80,8 @@ class NowPlayingAdapter(private var context: Context , private var movies: Mutab
         }
     }
 
-    fun remove(city: Results) {
-        val position = movies!!.indexOf(city)
-        if (position > -1) {
-            movies!!.removeAt(position)
-            notifyItemRemoved(position)
-        }
-    }
-
-    fun clear() {
-        isLoadingAdded = false
-        while (itemCount > 0) {
-            remove(getItem(0))
-        }
-    }
-
-    val isEmpty: Boolean
-        get() = itemCount == 0
-
     fun addLoadingFooter() {
         isLoadingAdded = true
-      //  add(Movie())
     }
 
     fun removeLoadingFooter() {
@@ -120,20 +97,16 @@ class NowPlayingAdapter(private var context: Context , private var movies: Mutab
     fun getItem(position: Int): Results {
         return movies!![position]
     }
-    /*
-   View Holders
-   _________________________________________________________________________________________________
-    */
-    /**
-     * Main list's content ViewHolder
-     */
-    protected inner class MovieVH(itemView: View) : ViewHolder(itemView) {
-     //   val textView: TextView
 
-        init {
-         //   textView = itemView.findViewById(R.id.item_text)
-        }
+    private inner class MovieVH(itemView: View) : ViewHolder(itemView) {
+        val backgroundImage: ImageView = itemView.findViewById(R.id.background_img)
+        val tvTitle: TextView = itemView.findViewById(R.id.tv_title)
+        val tvReleaseDate: TextView = itemView.findViewById(R.id.tv_release_date)
+        val tvPercent: TextView = itemView.findViewById(R.id.tv_percent)
+        val progressBar: ProgressBar = itemView.findViewById(R.id.circularProgressbar)
     }
+
+    private inner class LoadingVH(itemView: View) : ViewHolder(itemView)
 
 
 }
